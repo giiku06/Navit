ba2812bbc77c1f6a627a7f092edf929b2a0defe9
coa3 <lovecoal928@gmail.com>
2023-07-14 14:31:27 +0900

    feat: apiで付近の駅情報をフェッチ

--- app/src/main/java/com/example/giiku06application/MainActivity.java
+++ app/src/main/java/com/example/giiku06application/MainActivity.java
@@ -9,11 +9,36 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final String API_HOST = "https://navitime-transport.p.rapidapi.com/transport_node/around?coord=35.166816%2C136.952957&limit=2&term=60&datum=wgs84&coord_unit=degree&walk_speed=5";
+    private static final String API_KEY = "ee6978f011mshb71e6fca9594701p1c2bcbjsn3aa55c5144d5";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://navitime-transport.p.rapidapi.com/transport_node/id?id=00005077&datum=wgs84&coord_unit=degree")
+                .get()
+                .addHeader("X-RapidAPI-Key", "ee6978f011mshb71e6fca9594701p1c2bcbjsn3aa55c5144d5")
+                .addHeader("X-RapidAPI-Host", "navitime-transport.p.rapidapi.com")
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            Log.d("res", "onCreate: "+response);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
--- app/src/main/java/com/example/giiku06application/MainAppWidget.java
+++ app/src/main/java/com/example/giiku06application/MainAppWidget.java
@@ -5,6 +5,7 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ListView;
@@ -14,6 +15,16 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 /**
  * Implementation of App Widget functionality.
  */
@@ -21,26 +32,25 @@ public class MainAppWidget extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        for(int appWidgetId : appWidgetIds){
-            //listに渡すデータの作成
-            String[] ride_time = {"11 : 00", "12 : 00", "13 : 00"};
-            String[] drop_off_time = {"11 : 30", "12 : 30", "13 : 30"};
-            String[] route_text = {"東山線", "鶴舞線", "桜通線"};
-            String[] last_station_text = {"高畑行", "上小田井行", "太閤通行"};
-            //データの格納
-            Bundle bundle = new Bundle();
-            bundle.putStringArray("ride_time", ride_time);
-            bundle.putStringArray("drop_off_time", drop_off_time);
-            bundle.putStringArray("route_text", route_text);
-            bundle.putStringArray("last_station_text", last_station_text);
-
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.main_app_widget);
-            Intent intent = new Intent(context, MyWidgetService.class);
-            intent.putExtras(bundle);
-            views.setRemoteAdapter(R.id.widget_list_view, intent);
-            appWidgetManager.updateAppWidget(appWidgetId, views);
-        }
         super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+        //listに渡すデータの作成
+        String[] ride_time = {"11 : 00", "12 : 00", "13 : 00"};
+        String[] drop_off_time = {"11 : 30", "12 : 30", "13 : 30"};
+        String[] route_text = {"東山線", "鶴舞線", "桜通線"};
+        String[] last_station_text = {"高畑行", "上小田井行", "太閤通行"};
+        //データの格納
+        Bundle bundle = new Bundle();
+        bundle.putStringArray("ride_time", ride_time);
+        bundle.putStringArray("drop_off_time", drop_off_time);
+        bundle.putStringArray("route_text", route_text);
+        bundle.putStringArray("last_station_text", last_station_text);
+
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.main_app_widget);
+        Intent intent = new Intent(context, MyWidgetService.class);
+        intent.putExtras(bundle);
+        views.setRemoteAdapter(R.id.widget_list_view, intent);
+        appWidgetManager.updateAppWidget(appWidgetIds, views);
     }
 
     @Override
